e019158dbec168c6bb19f20d3c48cd35551f41a6
Lu Cho-Ching <choching.work@gmail.com>
2015-12-02 17:04:14 +0800

    simple scala cluster

--- build.sbt
+++ build.sbt
@@ -5,9 +5,11 @@ version := "1.0"
 scalaVersion := "2.11.7"
 
 libraryDependencies ++= {
-  val akkaV = "2.4.0"
+  val akkaV = "2.4.1"
   Seq(
     "com.typesafe.akka" %% "akka-actor" % akkaV,
-    "com.typesafe.akka" %% "akka-remote" % akkaV
+    "com.typesafe.akka" %% "akka-remote" % akkaV,
+    "com.typesafe.akka" %% "akka-cluster" % akkaV,
+    "com.typesafe.akka" % "akka-cluster-metrics_2.11" % akkaV
   )
 }
---
+++ src/main/scala/com/example/ClusterFrontendActor.scala
@@ -0,0 +1,47 @@
+package com.example
+
+import akka.actor.{Props, ActorLogging, Actor, ActorSystem}
+import akka.cluster.{MemberStatus, Cluster}
+import akka.cluster.ClusterEvent._
+import com.typesafe.config.ConfigFactory
+
+
+class ClusterFrontendActor extends Actor with ActorLogging {
+  val cluster = Cluster(context.system)
+
+  override def preStart(): Unit = {
+    cluster.subscribe(self, initialStateMode = InitialStateAsEvents,
+      classOf[MemberEvent], classOf[UnreachableMember])
+  }
+
+  override def postStop(): Unit = cluster.unsubscribe(self)
+
+  def receive = {
+    case MemberUp(member) =>
+      log.warning("Member is Up: {}", member.address)
+    case UnreachableMember(member) =>
+      log.warning("Member detected as unreachable: {}", member)
+    case MemberRemoved(member, previousStatus) =>
+      log.warning("Member is Removed: {} after {}",
+        member.address, previousStatus)
+    case _: MemberEvent =>
+  }
+}
+
+
+object ClusterFrontendActor {
+  //activator "runMain com.example.ClusterFrontendActor 0"
+  def main(args: Array[String]) {
+    startup(args(0))
+  }
+
+  def startup(port: String): Unit = {
+    val sourceConf = ConfigFactory.load("cluster_configuration")
+
+    val config = ConfigFactory.defaultOverrides()
+      .withFallback(ConfigFactory.parseString("akka.remote.netty.tcp.port=" + port))
+      .withFallback(sourceConf)
+    val system = ActorSystem("ClusterSystem", config)
+    system.actorOf(Props[ClusterFrontendActor], name = "clusterFrontendActor")
+  }
+}
